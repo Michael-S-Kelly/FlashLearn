863254aaaf1e17c6bbe88c95f0e510441b0c563d
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-19 05:40:59 -0700

    Commit before going to School

---
+++ FlashLearn/Models/UserCourse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashLearn.Models
+{
+    public class UserCourse
+    {
+        public int UserID { get; set; }
+        public int CourseID { get; set; }
+
+        public ICollection<User> Users { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
